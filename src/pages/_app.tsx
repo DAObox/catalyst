8c14809fbf5a0c4af5f340fc8378be2b97aaf691
supreme2580 <victoromorogbe69@gmail.com>
2023-05-27 22:00:08 +0100

    suprememods

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import { type AppType } from "next/dist/shared/lib/utils";
-import { AppShell } from "../components/layout/AppShell";
 
 import "@/styles/globals.css";
 import '@rainbow-me/rainbowkit/styles.css';
@@ -9,12 +8,12 @@ import {
   RainbowKitProvider,
 } from '@rainbow-me/rainbowkit';
 import { configureChains, createConfig, WagmiConfig } from 'wagmi';
-import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
+import { polygon } from 'wagmi/chains';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
 
 const { chains, publicClient } = configureChains(
-  [mainnet, polygon, optimism, arbitrum],
+  [polygon],
   [
     alchemyProvider({ apiKey: process.env.ALCHEMY_ID || "" }),
     publicProvider()
@@ -22,8 +21,8 @@ const { chains, publicClient } = configureChains(
 );
 
 const { connectors } = getDefaultWallets({
-  appName: 'My RainbowKit App',
-  projectId: 'YOUR_PROJECT_ID',
+  appName: 'Catalyst',
+  projectId: 'CATALYST',
   chains
 });
 
