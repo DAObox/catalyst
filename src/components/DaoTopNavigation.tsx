61c3efb7128a77a6a50b86e04f1e2da145d1cca9
supreme2580 <victoromorogbe69@gmail.com>
2023-05-31 10:17:46 +0100

    suprememods

--- src/components/DaoTopNavigation.tsx
+++ src/components/DaoTopNavigation.tsx
@@ -1,25 +1,33 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useConnectModal } from "@rainbow-me/rainbowkit";
 import CustomConnectButton from "./buttons/CustomConenctButton";
+import Link from "next/link";
 
 export default function DaoTopNavigation() {
     const { openConnectModal } = useConnectModal();
     return (
         <div className="flex items-center justify-between text-white">
-            <div className="flex-1 absolute inset-x-0 flex justify-center">
-                <div className="flex space-x-5">
-                    <div>
-                        <p>Governance</p>
-                    </div>
-                    <div>
-                        <p>Treasury</p>
-                    </div>
-                    <div>
-                        <p>Member</p>
-                    </div>
+            <div className="flex-1 flex justify-between">
+                <div className={`flex space-x-5 flex-1 justify-center ${!openConnectModal && "ml-40"}`}>
+                    <Link href={"/"} className="hover:text-green">
+                        Dashboard
+                    </Link>
+                    <Link href={"/"} className="hover:text-green">
+                        Governance
+                    </Link>
+                    <Link href={"/"} className="hover:text-green">
+                        Treasury
+                    </Link>
+                    <Link href={"/"} className="hover:text-green">
+                        Members
+                    </Link>
+                    <Link href={"/"} className="hover:text-green">
+                        Settings
+                    </Link>
                 </div>                
             </div>
-            <div className="flex-1 flex justify-end z-20">
+            <div className="flex justify-end z-20">
                 {
                     openConnectModal ? <CustomConnectButton /> : <ConnectButton />
                 }
