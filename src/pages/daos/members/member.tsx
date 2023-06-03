7ecc1c34eb2bbd456178e9cbdaeb7bc12631bc1d
supreme2580 <victoromorogbe69@gmail.com>
2023-06-03 04:40:26 +0100

    suprememods

--- src/pages/daos/members/member.tsx
+++ src/pages/daos/members/member.tsx
@@ -27,8 +27,31 @@ export default function Member() {
                                     <h3 className="text-ash text-lg">@israelrex.lens</h3>
                                 </div>
                                 <div className="w-full flex space-x-10">
-                                    <div className="flex-1">
+                                    <div className="flex-1 space-y-4">
                                         <p className="tex-medium-gray">{truncate("Crafting decentralized experiences, bridging creativity with technology. Designing the future of web3, where user empowerment and blockchain innovation merge seamlessly. Embrace the revolution, join me on this transformative journey!", 250)}</p>
+                                        <div className="flex items-center space-x-2">
+                                            <div className="text-xs text-light-black">Mutual Communities</div>
+                                            <div className="flex -space-x-2 items-center mt-1.5">
+                                                <div>
+                                                    <Image src={"/logos/aragon.png"} width={32} height={32} alt="aragon icon" />
+                                                </div>
+                                                <div>
+                                                    <Image src={"/logos/aragon.png"} width={32} height={32} alt="aragon icon" />
+                                                </div>
+                                                <div>
+                                                    <Image src={"/logos/aragon.png"} width={32} height={32} alt="aragon icon" />
+                                                </div>
+                                                <div>
+                                                    <Image src={"/logos/aragon.png"} width={32} height={32} alt="aragon icon" />
+                                                </div>
+                                                <div>
+                                                    <Image src={"/logos/aragon.png"} width={32} height={32} alt="aragon icon" />
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <p className="text-sm text-green font-medium">+4 more</p>
+                                            </div>
+                                        </div>
                                     </div>
                                     <div className="max-w-sm w-full">
                                         <div className="w-full space-y-4">
