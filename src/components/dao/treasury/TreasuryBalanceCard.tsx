e83229ca0e75ea2eb3248c263cba1452e6efc103
supreme2580 <victoromorogbe69@gmail.com>
2023-06-03 18:01:48 +0100

    suprememods

--- src/components/dao/treasury/TreasuryBalanceCard.tsx
+++ src/components/dao/treasury/TreasuryBalanceCard.tsx
@@ -1,37 +1,27 @@
 import NewTransferButton from "@/components/buttons/NewTransferButton";
 import { QuestionMarkCircleIcon } from "@heroicons/react/24/solid";
 import { Tooltip } from "@material-tailwind/react";
-import TokensList from "./TokensList";
-import LastTransactions from "./LastTransactions";
 
 export default function TreasuryBalanceCard() {
-    return(
-        <div className="w-full space-y-10">
-            <div className="w-full flex items-center justify-between bg-gray rounded-lg p-10">
-                <div className="space-y-2">
-                    <div className="flex items-center space-x-1">
-                        <div>
-                            <p className="text-light-black">Total Balance</p>
-                        </div>
-                        <div>
-                            <Tooltip content="Question mark">
-                                <QuestionMarkCircleIcon className="w-5 h-5 text-light-black hover:Cursor-pointer" />
-                            </Tooltip>
-                        </div>
+    return (
+        <div className="w-full flex items-center justify-between bg-gray rounded-lg p-10">
+            <div className="space-y-2">
+                <div className="flex items-center space-x-1">
+                    <div>
+                        <p className="text-light-black">Total Balance</p>
                     </div>
                     <div>
-                        <h1 className="text-4xl font-bold text-light-gray">$40,580.70 <span className="text-xs font-normal">USDC</span></h1>
+                        <Tooltip content="Question mark">
+                            <QuestionMarkCircleIcon className="w-5 h-5 text-light-black hover:Cursor-pointer" />
+                        </Tooltip>
                     </div>
                 </div>
                 <div>
-                    <NewTransferButton />
+                    <h1 className="text-4xl font-bold text-light-gray">$40,580.70 <span className="text-xs font-normal">USDC</span></h1>
                 </div>
             </div>
             <div>
-                <TokensList />
-            </div>
-            <div>
-                <LastTransactions />
+                <NewTransferButton />
             </div>
         </div>
     )
