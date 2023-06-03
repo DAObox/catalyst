b11690ee62e28c86648474df9a7dfc1392c3b225
supreme2580 <victoromorogbe69@gmail.com>
2023-06-03 13:40:54 +0100

    suprememods

--- src/components/dao/members/detail/Activity.tsx
+++ src/components/dao/members/detail/Activity.tsx
@@ -1,14 +1,15 @@
 import MemberDetailsCardPost from "./activity/MembersDetailsCardPost"
 import CommentInPostCard from "./activity/CommentInPostCard"
 import LikedInPostCard from "./activity/LikedInPostCard"
+import ActivityDivider from "./activity/ActivityDivider"
 
 export default function Activity() {
     return (
         <div className="space-y-5">
             <MemberDetailsCardPost />
-            <div className="bg-gray-border h-[1px] w-full" />
+            <ActivityDivider />
             <CommentInPostCard />
-            <div className="bg-gray-border h-[1px] w-full" />
+            <ActivityDivider />
             <LikedInPostCard />
         </div>
     )
