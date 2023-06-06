ead6aeb66e725b7234bde3c15284384eb7907428
supreme2580 <victoromorogbe69@gmail.com>
2023-06-06 12:49:40 +0100

    suprememods

---
+++ src/components/buttons/AddCommentButton.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@material-tailwind/react";
+
+export default function AddCommentButton() {
+    return(
+        <Button className="bg-trans-gray text-light-gray">Submit</Button>
+    )
+}
--- src/components/dao/governance/AddComment.tsx
+++ src/components/dao/governance/AddComment.tsx
@@ -1,9 +1,12 @@
+import AddCommentButton from "@/components/buttons/AddCommentButton";
+
 export default function AddComment() {
     return(
         <div className="w-full p-10 rounded-xl bg-gray border border-gray-border space-y-5">
             <h3 className="text-xl font-bold text-lighter-gray">Add comment</h3>
-            <textarea className="w-full h-32 bg-transparent border border-trans-gray outline-0 p-2 placeholder:text-medium-gray text-medium-gray" 
+            <textarea className="w-full h-32 bg-transparent border border-trans-gray outline-0 p-2 placeholder:text-medium-gray text-medium-gray rounded-lg" 
             placeholder="Say what is in your mind" />
+            <AddCommentButton />
         </div>
     )
 }
