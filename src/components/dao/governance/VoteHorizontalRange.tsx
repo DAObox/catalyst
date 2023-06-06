71ec35089e4d6a17f698c44ade890af3889126e7
supreme2580 <victoromorogbe69@gmail.com>
2023-06-06 12:22:45 +0100

    suprememods

--- src/components/dao/governance/VoteHorizontalRange.tsx
+++ src/components/dao/governance/VoteHorizontalRange.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 import { type VoteHorizontalRangeProps } from "typings/typings";
+import Dot from "../members/detail/activity/Dot";
 
 export default function VoteHorizontalRange({ _for, against, abstain }: VoteHorizontalRangeProps) {
     return (
@@ -15,6 +16,19 @@ export default function VoteHorizontalRange({ _for, against, abstain }: VoteHori
                 <div className={`h-4 w-[${abstain}%] bg-light-black rounded-r-full ${_for === 0 && "rounded-l-full"} ${against === 0 && "rounded-r-full"}`} style={{
                     "width": `${abstain}%`
                 }} />
+            </div>
+            <div className="flex items-center space-x-1.5">
+                <div>
+                    <p className="text-light-black">{_for}% For</p>
+                </div>
+                <Dot />
+                <div>
+                    <p className="text-light-black">{against}% Against</p>
+                </div>
+                <Dot />
+                <div>
+                    <p className="text-light-black">{abstain}% Abstain</p>
+                </div>
             </div>
         </div>
     )
