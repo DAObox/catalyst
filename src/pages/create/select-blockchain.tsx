3391a34d12283df89e855b3df5853eade46dac50
supreme2580 <victoromorogbe69@gmail.com>
2023-06-08 16:08:40 +0100

    suprememods

--- src/pages/create/select-blockchain.tsx
+++ src/pages/create/select-blockchain.tsx
@@ -4,14 +4,17 @@ import CreateDaoAppShell from "@/components/create-dao/CreateDaoAppShell";
 export default function SelectBlockchain() {
     return (
         <CreateDaoAppShell>
-            <div className="w-full bg-gray border border-gray-border p-10 rounded-xl">
-                <div className="space-y-2">
-                    <h1 className="text-lighter-gray text-4xl">Create Your DAO</h1>
-                    <p className="text-light-gray">Create your DAO description</p>
-                    <div>
-                        <CreateDaoAccordion />
+            <div className="space-y-10">
+                <div className="w-full bg-gray border border-gray-border p-10 rounded-xl">
+                    <div className="space-y-2">
+                        <h1 className="text-lighter-gray text-4xl">Create Your DAO</h1>
+                        <p className="text-light-gray">Create your DAO description</p>
+                        <div>
+                            <CreateDaoAccordion />
+                        </div>
                     </div>
                 </div>
+                <div className="w-full p-5 rounded-lg bg-trans-gray"></div> 
             </div>
         </CreateDaoAppShell>
     )
