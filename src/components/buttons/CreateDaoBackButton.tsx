a7c755b4455046b982b781777ef8dbbb82c5863c
supreme2580 <victoromorogbe69@gmail.com>
2023-06-12 17:51:45 +0100

    suprememods

--- src/components/buttons/CreateDaoBackButton.tsx
+++ src/components/buttons/CreateDaoBackButton.tsx
@@ -9,7 +9,7 @@ export default function CreateDaoBackButton() {
     const step = useRecoilValue(createDaoStep)
     const router = useRouter()
     return(
-        <Button onClick={() => step + 1 !== 6 && router.push(createDaoStepNavigation[step-2] || "")} 
+        <Button onClick={() => step !== 5 && router.push(createDaoStepNavigation[step-2] || "")} 
         className="bg-transparent border border-lighter-gray px-5 py-3.5 rounded-xl text-lighter-gray flex items-center gap-3"
         disabled={step - 1 == 0 ? true : false}>Back</Button>
     )
--- src/components/create-dao/CreateDaoPageStatus.tsx
+++ src/components/create-dao/CreateDaoPageStatus.tsx
@@ -20,8 +20,8 @@ export default function CreateDaoPageStatus({ page }: CreateDaoPageStatusProps) 
                 <div className="space-y-5">
                     <SelectBlockchain current={step == 1 && router.pathname == createDaoStepNavigation[0] ? true : false} />
                     <AboutDao current={step == 2 && router.pathname == createDaoStepNavigation[1] ? true : false} />
-                    <ConnectLensProfile current={step == 3 && router.pathname == createDaoStepNavigation[2] || router.pathname == createDaoStepNavigation[3] ? true : false} />
-                    <Governance current={step == 4 && router.pathname == createDaoStepNavigation[5] ? true : false} />
+                    <ConnectLensProfile current={step == 3 && router.pathname == createDaoStepNavigation[2] || router.pathname == createDaoStepNavigation[4] ? true : false} />
+                    <Governance current={step == 4 && router.pathname == createDaoStepNavigation[3] ? true : false} />
                 </div>
             </div>
         </div>
