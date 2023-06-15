ddfef5809c1ac7d65a9689177c1428bb7bd68dd9
supreme2580 <victoromorogbe69@gmail.com>
2023-06-15 13:05:34 +0100

    suprememods

--- src/components/dao/create-dao/steps/Governance.tsx
+++ src/components/dao/create-dao/steps/Governance.tsx
@@ -1,6 +1,6 @@
-import { type CreateDaoStepProps } from "typings/typings";
+import { type StepProps } from "typings/typings";
 
-export default function Governance({ current }: CreateDaoStepProps) {
+export default function Governance({ current }: StepProps) {
     return(
         <div className={`max-w-full px-10 py-14 ${current ? "bg-trans-gray" : "border border-medium-gray"} text-lighter-gray rounded-lg`}>
             <p className="text-xl text-center">Governance</p>
