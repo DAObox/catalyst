f9d8877e2a6bc1b3e1c684761640f0b6d2448494
supreme2580 <victoromorogbe69@gmail.com>
2023-06-17 20:48:06 +0100

    suprememods

--- src/components/dao/create-dao/Mainnet.tsx
+++ src/components/dao/create-dao/Mainnet.tsx
@@ -4,7 +4,7 @@ export default function Mainnet() {
     return (
         <div className="grid grid-cols-4 gap-8">
         <BlockchainBlock name="Polygon" icon="/logos/polygon.png" />
-        <BlockchainBlock name="Ethereum" icon="/logos/ethereum" />
+        <BlockchainBlock name="Ethereum" icon="/logos/ethereum.png" />
         </div>
     )
 }
