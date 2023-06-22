cc2d635a35f754084119603a7acba6130a3b9e37
pythonpete32 <aaron@blackscale.io>
2023-06-22 12:50:08 +0400

    add providers

---
+++ src/lib/wagmi.ts
@@ -0,0 +1,24 @@
+import { w3mConnectors, w3mProvider } from "@web3modal/ethereum";
+import { configureChains, createConfig } from "wagmi";
+import { polygon, polygonMumbai } from "wagmi/chains";
+
+export const walletConnectProjectId =
+  process.env.NEXT_PUBLIC_WALLET_CONNECT_ID ?? "";
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [polygon, polygonMumbai],
+  [w3mProvider({ projectId: walletConnectProjectId })]
+);
+
+export const config = createConfig({
+  autoConnect: true,
+  connectors: w3mConnectors({
+    chains,
+    projectId: walletConnectProjectId,
+    version: 2,
+  }),
+  publicClient,
+  webSocketPublicClient,
+});
+
+export { chains };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,44 +1,14 @@
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { type AppType } from "next/dist/shared/lib/utils";
 import "@/styles/globals.css";
 import { useEffect, useState } from "react";
-import { RecoilRoot } from "recoil"
-import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
-import { Web3Modal } from '@web3modal/react'
-import { configureChains, createConfig, WagmiConfig } from 'wagmi'
-import { arbitrum, mainnet, polygon } from 'wagmi/chains'
-
-const chains = [arbitrum, mainnet, polygon]
-const projectId = 'CATALYST'
-
-const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors: w3mConnectors({ projectId, version: 1, chains }),
-  publicClient
-})
-const ethereumClient = new EthereumClient(wagmiConfig, chains)
+import { Providers } from "./providers";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   const [mounted, setMounted] = useState(false);
   useEffect(() => {
-      setMounted(true);
+    setMounted(true);
   }, []);
-  return (
-    <>
-      <WagmiConfig config={wagmiConfig}>
-        <RecoilRoot>
-          {mounted && <Component {...pageProps} />}
-        </RecoilRoot>
-      </WagmiConfig>
-      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} themeVariables={{ 
-        '--w3m-accent-color': '#1E293B',
-        '--w3m-background-color': '#101725'
-      }} />
-    </>
-  );
+  return <Providers>{mounted && <Component {...pageProps} />}</Providers>;
 };
 
 export default MyApp;
