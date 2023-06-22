61d8a900cf79343293d0a9902bc462de1d1b4523
pythonpete32 <aaron@blackscale.io>
2023-06-22 13:15:51 +0400

    add `useNewDAO` & encoders

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -2,11 +2,31 @@ import Navbar from "@/components/Navbar";
 import type { NextPage } from "next";
 import { useFetchDaos } from "@/hooks/useFetchDaos";
 import { Button } from "@material-tailwind/react";
+import { useNewDao } from "@/hooks/useNewDao";
+import { encodeLensVotingPlugin } from "@/lib/encoders/EncodeLensVotingPlugin";
+
+const votingToken = "0xD3596C81FcAb699192dc79C8e25f1362E3dFf89A";
 
 const Test: NextPage = () => {
-  const { data: daos } = useFetchDaos({ limit: 3 });
+  useFetchDaos({ limit: 3 });
 
-  console.log(daos);
+  const name = `test-${Math.floor(Math.random() * 1000)}`;
+
+  const { mutate, creationStatus, data, error } = useNewDao({
+    daoMetadata: {
+      name,
+      description: "some description",
+      links: [],
+    },
+    ensSubdomain: name,
+    plugins: [
+      encodeLensVotingPlugin({
+        lensFollowNFT: votingToken,
+        network: "polygon",
+      }),
+    ],
+  });
+
   return (
     <div className="h-full w-full overflow-x-clip overflow-y-scroll bg-gradient-to-r from-[#1E203C] to-[#11121B]">
       <main className="relative flex h-full min-h-screen w-full justify-center">
@@ -14,8 +34,35 @@ const Test: NextPage = () => {
           <Navbar />
           <div className="absolute top-32 w-full space-y-10">
             <div>
-              <Button variant="outlined">Deploy DAO</Button>
+              <Button
+                variant="outlined"
+                onClick={() => mutate?.()}
+                disabled={["pinningMetadata", "creatingDao"].includes(
+                  creationStatus
+                )}
+              >
+                Deploy DAO
+              </Button>
             </div>
+            <h3>Status: {creationStatus}</h3>
+            <h3>
+              data:
+              <pre>
+                {JSON.stringify(
+                  data,
+                  (_, v) => (typeof v === "bigint" ? v.toString() : v),
+                  2
+                )}
+              </pre>
+            </h3>
+            {data && (
+              <a
+                href={`https://app.aragon.org/#/daos/goerli/${data.daoAddress}/dashboard`}
+              >
+                Goto Dao
+              </a>
+            )}
+            {error && <h3>Error: {error?.message}</h3>}
           </div>
         </div>
       </main>
--- typings/typings.ts
+++ typings/typings.ts
@@ -1,4 +1,9 @@
-import { UseQueryOptions } from "@tanstack/react-query";
+import { type DaoMetadata, type IPluginInstallItem } from "@aragon/sdk-client";
+import {
+  type UseMutationOptions,
+  type UseMutationResult,
+  type UseQueryOptions,
+} from "@tanstack/react-query";
 import { type SetterOrUpdater } from "recoil";
 
 export interface CustomOptionsProps {
@@ -179,3 +184,87 @@ export type QueryConfig<TData, TSelectData = TData> = Pick<
   | "queryKey"
   | "select"
 >;
+
+/**
+ * A configuration object used for customizing the behavior of a useMutation hook.
+ * It extends the UseMutationOptions from React Query by adding an additional onTransaction callback.
+ *
+ * @template Data - The type of the data returned by the mutation.
+ * @template Error - The type of the error returned by the mutation.
+ * @template Variables - The type of the variables used by the mutation (default to void).
+ *
+ * @property onError - A function that is called when the mutation encounters an error.
+ * @property onMutate - A function that is called before the mutation function is executed. It receives the same variables as the mutation function, and its returned value is passed to both onError and onSettled functions in the event of a mutation failure.
+ * @property onSettled - A function that is called when the mutation is either successfully fetched or encounters an error.
+ * @property onSuccess - A function that is called when the mutation is successful. It is passed the mutation's result.
+ * @property onTransaction - An optional callback function that is called with the transaction hash (txHash) when a new transaction is created.
+ */
+export type MutationConfig<Data, Error, Variables = void> = {
+  onError?: UseMutationOptions<Data, Error, Variables>["onError"];
+  onMutate?: UseMutationOptions<Data, Error, Variables>["onMutate"];
+  onSettled?: UseMutationOptions<Data, Error, Variables>["onSettled"];
+  onSuccess?: UseMutationOptions<Data, Error, Variables>["onSuccess"];
+};
+
+/**
+ * @typedef {object} NewDaoParams
+ * @property {MutationConfig<NewDaoReturnData, Error>} config - The configuration for the mutation.
+ * @property {DaoMetadata} daoMetadata - The metadata for the new DAO.
+ * @property {string} [daoUri] - The URI for the new DAO.
+ * @property {string} ensSubdomain - The ENS subdomain for the new DAO.
+ * @property {string} [trustedForwarder] - The trusted forwarder address for the new DAO.
+ * @property {IPluginInstallItem[]} plugins - The plugins to be installed in the new DAO.
+ * @property {(txHash: string) => void} [onCreateDaoTransaction] - A callback to be called when the DAO creation transaction is created.
+ */
+export type NewDaoParams = MutationConfig<NewDaoReturnData, Error> & {
+  daoMetadata: DaoMetadata;
+  daoUri?: string;
+  ensSubdomain: string;
+  trustedForwarder?: string;
+  plugins: IPluginInstallItem[];
+  onCreateDaoTransaction?: (txHash: string) => void;
+};
+
+/**
+ * @typedef {object} NewDaoReturnData
+ * @property {string|null} daoAddress - The address of the created DAO.
+ * @property {string|null} daoTxHash - The transaction hash of the created DAO.
+ */
+export type NewDaoReturnData = {
+  daoAddress: string | null;
+  daoTxHash: string | null;
+};
+
+/**
+ * Enum representing the possible statuses of a new DAO creation process.
+ * @readonly
+ * @enum {string}
+ */
+export enum NewDaoStatus {
+  IDLE = "idle",
+  PINNING_METADATA = "pinningMetadata",
+  CREATING_DAO = "creatingDao",
+  SUCCESS = "success",
+  ERROR = "error",
+}
+
+/**
+ * @typedef {object} NewDaoReturnType
+ * @extends {UseMutationResult<NewDaoReturnData, Error, void, unknown>}
+ * @property {string|null} daoAddress - The address of the created DAO.
+ * @property {string|null} daoTxHash - The transaction hash of the created DAO.
+ * @property {NewDaoStatus} creationStatus - The current status of the new DAO creation process.
+ */
+export type NewDaoReturnType = UseMutationResult<
+  {
+    daoAddress: string | null;
+    daoTxHash: string | null;
+  },
+  Error,
+  void,
+  unknown
+> & {
+  daoAddress: string | null;
+  daoTxHash: string | null;
+  creationStatus: NewDaoStatus;
+};
